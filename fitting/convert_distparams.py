efd01b561786368a48bc5485787e22ac7b98625e
David Stansby <dstansby@gmail.com>
2017-08-29 13:46:53 +0100

    Add probe loop when converting to .csv

--- fitting/convert_distparams.py
+++ fitting/convert_distparams.py
@@ -9,45 +9,45 @@ from config import get_dirs
 output_dir = get_dirs()
 
 
-probe = '2'
-for year in range(1976, 1981):
-    fdir = os.path.join(output_dir, 'helios' + probe, 'fits', str(year))
-    if not os.path.exists(os.path.join(fdir, 'csv')):
-        os.makedirs(os.path.join(fdir, 'csv'))
-    for doy in range(1, 367):
-        print(probe, year, doy)
-        hdf_fname = 'h{}_{}_{:03}_3D_fits.h5'.format(probe, year, doy)
-        csv_fname = 'h{}_{}_{:03}_3D_fits.csv'.format(probe, year, doy)
+for probe in ['1', '2']:
+    for year in range(1974, 1986):
+        fdir = os.path.join(output_dir, 'helios' + probe, 'fits', str(year))
+        if not os.path.exists(os.path.join(fdir, 'csv')):
+            os.makedirs(os.path.join(fdir, 'csv'))
+        for doy in range(1, 367):
+            print(probe, year, doy)
+            hdf_fname = 'h{}_{}_{:03}_3D_fits.h5'.format(probe, year, doy)
+            csv_fname = 'h{}_{}_{:03}_3D_fits.csv'.format(probe, year, doy)
 
-        hdf_file = os.path.join(fdir, hdf_fname)
-        csv_file = os.path.join(fdir, 'csv', csv_fname)
-        try:
-            data = pd.read_hdf(hdf_file)
-        except Exception as err:
-            print('No data available')
-            continue
-        # Convert all bad error codes to error code 3
-        data.loc[data['Status'] > 2, 'Status'] = 3
-        # Convert B instrument column to int
-        data['B instrument'] = data['B instrument'].astype(int)
+            hdf_file = os.path.join(fdir, hdf_fname)
+            csv_file = os.path.join(fdir, 'csv', csv_fname)
+            try:
+                data = pd.read_hdf(hdf_file)
+            except Exception as err:
+                print('No data available')
+                continue
+            # Convert all bad error codes to error code 3
+            data.loc[data['Status'] > 2, 'Status'] = 3
+            # Convert B instrument column to int
+            data['B instrument'] = data['B instrument'].astype(int)
 
-        data = data[['B instrument', 'Bx', 'By', 'Bz',
-                     'Ion instrument', 'Status', 'Tp_par', 'Tp_perp',
-                     'carrot', 'r_sun', 'clat', 'clong', 'earth_he_angle',
-                     'n_p', 'sigma B', 'vp_x', 'vp_y', 'vp_z',
-                     'vth_p_par', 'vth_p_perp']]
+            data = data[['B instrument', 'Bx', 'By', 'Bz',
+                         'Ion instrument', 'Status', 'Tp_par', 'Tp_perp',
+                         'carrot', 'r_sun', 'clat', 'clong', 'earth_he_angle',
+                         'n_p', 'sigma B', 'vp_x', 'vp_y', 'vp_z',
+                         'vth_p_par', 'vth_p_perp']]
 
-        def sigfigs(x, n):
-            return '%s' % float(('%.' + str(n) + 'g') % x)
+            def sigfigs(x, n):
+                return '%s' % float(('%.' + str(n) + 'g') % x)
 
-        def sigfigvars(data, varbls, n):
-            data[varbls] = data[varbls].applymap(lambda x: sigfigs(x, n))
-            return data
+            def sigfigvars(data, varbls, n):
+                data[varbls] = data[varbls].applymap(lambda x: sigfigs(x, n))
+                return data
 
-        data = sigfigvars(data, ['Bx', 'By', 'Bz', 'sigma B'], 6)
-        data = sigfigvars(data, ['Tp_perp', 'Tp_par'], 6)
-        data = sigfigvars(data, ['vp_x', 'vp_y', 'vp_z'], 6)
-        data = sigfigvars(data, ['vth_p_par', 'vth_p_perp'], 6)
-        data = sigfigvars(data, ['n_p'], 6)
+            data = sigfigvars(data, ['Bx', 'By', 'Bz', 'sigma B'], 6)
+            data = sigfigvars(data, ['Tp_perp', 'Tp_par'], 6)
+            data = sigfigvars(data, ['vp_x', 'vp_y', 'vp_z'], 6)
+            data = sigfigvars(data, ['vth_p_par', 'vth_p_perp'], 6)
+            data = sigfigvars(data, ['n_p'], 6)
 
-        data.to_csv(csv_file, na_rep='NaN')
+            data.to_csv(csv_file, na_rep='NaN')
