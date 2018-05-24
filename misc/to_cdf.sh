8cea9f5363d470f845dc42010092bb672934e131
David Stansby <dstansby@gmail.com>
2018-05-24 10:37:39 +0100

    Add header variable

--- misc/to_cdf.sh
+++ misc/to_cdf.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
+HEADER="/Users/dstansby/github/corefit/misc/helios_corefit.qfh"
 # This should be run from the root of directories in which the processed csv
 # files reside
 for i in $(find . -name '*.csv')
 do
   echo "$i"
-  Qtran -oc -h/home/dstansby/corefit/misc/helios_corefit.qfh $i
+  ./Qtran -oc -f -h$HEADER $i
   echo "$i done!"
 done
