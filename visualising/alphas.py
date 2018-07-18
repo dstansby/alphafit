5ba9b1048a48274bbd75600d7997f47bb4ecfdd7
David Stansby <dstansby@gmail.com>
2018-07-17 17:03:44 -0700

    Add temperatures

--- visualising/alphas.py
+++ visualising/alphas.py
@@ -15,10 +15,17 @@ starttime = datetime(1976, 4, 17, 0, 0, 0)
 endtime = starttime + timedelta(days=1)
 
 alphas = helpers.load_alphafit(probe, starttime, endtime)
-protons = helios.corefit(probe, starttime, endtime)
+protons = helios.corefit(probe, starttime, endtime).data
 
-fig, axs = plt.subplots(nrows=2, sharex=True)
-axs[0].plot(alphas['va_x'])
+fig, axs = plt.subplots(nrows=3, sharex=True)
+axs[0].plot(alphas.index, alphas['va_x'])
 axs[0].plot(protons['vp_x'])
 
+axs[1].plot(alphas['n_a'])
+axs[1].plot(protons['n_p'])
+axs[1].set_yscale('log')
+
+axs[2].plot(alphas['Ta_perp'])
+axs[2].plot(protons['Tp_perp'])
+axs[2].set_yscale('log')
 plt.show()
