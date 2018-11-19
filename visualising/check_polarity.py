8f500a22095337bf428b40e36f29b7319101e554
David Stansby <dstansby@gmail.com>
2018-11-19 17:00:43 +0000

    Update polarity check code for Helios 1

--- visualising/check_polarity.py
+++ visualising/check_polarity.py
@@ -7,12 +7,19 @@ import matplotlib.pyplot as plt
 import heliopy.data.imp as imp
 import heliopy.data.helios as helios
 
+hprobe = '1'
+starttime = datetime(1974, 12, 12)
+endtime = datetime(1975, 1, 1)
+
+'''hprobe = '2'
 starttime = datetime(1976, 1, 17)
-endtime = datetime(1976, 3, 1)
+endtime = datetime(1976, 2, 10)'''
 iprobe = '8'
 
-heliosdata = helios.mag_ness('2', starttime, endtime)
-heliosdata.data = heliosdata.data.resample('2H').mean()
+sixs = helios.mag_ness(hprobe, starttime, endtime)
+sixs.data = sixs.data.resample('2H').mean()
+fourhz = helios.mag_4hz(hprobe, starttime, endtime)
+fourhz.data = fourhz.data.resample('2H').mean()
 
 # Import IMP8 data in the solar wind. These data are in GSE coordinates,
 # which have the x-axis pointing towards the Sun
@@ -21,11 +28,22 @@ impdata.data = impdata.data.loc[impdata.data['sw_flag'] == 0]
 impdata.data = impdata.data.resample('2H').mean()
 
 # Compare data
-fig, axs = plt.subplots(nrows=2, sharex=True)
+fig, axs = plt.subplots(nrows=3, sharex=True)
 ax = axs[0]
 ax.plot(impdata.index, impdata.quantity('Bx_gse'))
 ax.plot(impdata.index, impdata.quantity('By_gse'))
+ax.plot(impdata.index, impdata.quantity('Bz_gse'))
+ax.set_title('IMP8 (GSE)')
 ax = axs[1]
-ax.plot(heliosdata.index, heliosdata.quantity('Bx'))
-ax.plot(heliosdata.index, heliosdata.quantity('By'))
+ax.plot(sixs.index, sixs.quantity('Bx'))
+ax.plot(sixs.index, sixs.quantity('By'))
+ax.plot(sixs.index, sixs.quantity('Bz'))
+ax.set_title('Helios 6s')
+ax = axs[2]
+ax.plot(fourhz.index, fourhz.quantity('Bx'))
+ax.plot(fourhz.index, fourhz.quantity('By'))
+ax.plot(fourhz.index, fourhz.quantity('Bz'))
+ax.set_title('Helios 4Hz')
+
+fig.suptitle('Helios {}'.format(hprobe))
 plt.show()
