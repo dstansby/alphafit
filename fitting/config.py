59c27203fa571a6e9dc28970cfe975b30c691b91
David Stansby <dstansby@gmail.com>
2018-12-17 10:41:03 +0000

    alpha > alphafit

--- fitting/config.py
+++ fitting/config.py
@@ -20,12 +20,12 @@ def get_dirs():
         raise FileNotFoundError('Output directory "{}" specified in '
                                 'config.ini does not exist'.format(output_dir))
 
-    alpha_code_dir = os.path.expanduser(
-        config['data_dirs']['alpha_code_dir'])
-    alpha_code_dir = pathlib.Path(alpha_code_dir)
-    if not os.path.isdir(alpha_code_dir):
+    alphafit_code_dir = os.path.expanduser(
+        config['data_dirs']['alphafit_code_dir'])
+    alphafit_code_dir = pathlib.Path(alphafit_code_dir)
+    if not os.path.isdir(alphafit_code_dir):
         raise FileNotFoundError('alphafit code directory "{}" specified in '
                                 'config.ini does not exist'.format(
-                                    alpha_code_dir))
+                                    alphafit_code_dir))
 
-    return output_dir, alpha_code_dir
+    return output_dir, alphafit_code_dir
