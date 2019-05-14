9b5adb4cdb2051cadcc239180f062d0538c95fc4
David Stansby <dstansby@gmail.com>
2019-05-14 13:24:43 +0100

    Log relative error

--- fitting/save_alpha_params.py
+++ fitting/save_alpha_params.py
@@ -289,8 +289,9 @@ def fit_single_dist(probe, time, dist3D, I1a, I1b, corefit, params):
     else:
         popt, pcov = result
         perr = np.sqrt(np.diag(pcov))
-        logger.info(f'Error is {perr}')
+        logger.info(f'Absolute error is {perr}')
         relerr = np.abs(perr) / popt
+        logger.info(f'Relative error is {relerr}')
         if np.any(relerr > 1):
             status = 10
         else:
