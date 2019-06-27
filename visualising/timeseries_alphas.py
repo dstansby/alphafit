e7c6ac69acaaa72c6b7a4794eb66804efe77ac21
David Stansby <dstansby@gmail.com>
2019-06-27 14:26:40 +0100

    Add extra plotting axes

--- visualising/timeseries_alphas.py
+++ visualising/timeseries_alphas.py
@@ -16,8 +16,8 @@ import vis_helpers as helpers
 import helioshelp
 
 probe = '1'
-starttime = datetime(1976, 3, 14)
-endtime = starttime + timedelta(days=10)
+starttime = datetime(1975, 3, 7)
+endtime = starttime + timedelta(days=20)
 
 alphas = helpers.load_alphafit(probe, starttime, endtime)
 protons = helios.corefit(probe, starttime, endtime, try_download=False).data
@@ -27,10 +27,11 @@ protons = helioshelp.calculate_derived(protons)
 alphas[['Bx', 'By', 'Bz', '|B|']] = protons[['Bx', 'By', 'Bz', '|B|']]
 alphas['Beta_par'] = alphas['Ta_par'] * const.k_B.value * alphas['n_a'] * 1e6 / (alphas['|B|']**2 * 1e-18 / (2 * const.mu0))
 alphas['Tani'] = alphas['Ta_perp'] / alphas['Ta_par']
+print(alphas.keys())
 
 akwargs = {'alpha': 1, 'color': 'C0', 's': 1}
 pkwargs = {'alpha': 0.5, 'color': 'C3', 's': 1}
-fig, axs = plt.subplots(nrows=6, sharex=True, figsize=(10, 12))
+fig, axs = plt.subplots(nrows=7, sharex=True, figsize=(10, 12))
 
 ax = axs[0]
 ax.scatter(alphas.index, alphas['va_x'], **akwargs)
@@ -60,19 +61,21 @@ ax.set_ylim(1e4, 1e7)
 ax.set_ylabel(r'$T_{\perp}$ (K)')
 
 ax = axs[4]
+ax.scatter(alphas.index, alphas['n_a'] / protons['n_p'], s=1, label=r'$n_{\alpha} / n_{p}$')
+ax.set_yscale('log')
+ax.set_ylim(1e-3, 1)
+
+ax = axs[5]
 ax.scatter(alphas.index, alphas['Ta_par'] / protons['Tp_par'], s=1, color='C1', label=r'$\parallel$')
 ax.scatter(alphas.index, alphas['Ta_perp'] / protons['Tp_perp'], s=1, color='C2', label=r'$\perp$')
 
 ax.legend()
 ax.set_yscale('log')
 ax.set_ylim(1e0, 1e2)
-ax.axhline(4, color='k', alpha=0.5)
+ax.axhline(1, color='k', alpha=0.5)
 # ax.set_ylabel(r'$T_{\alpha \perp}$')
 
-ax = axs[5]
-ax.plot(protons['r_sun'], color='k')
-ax.set_ylabel('r (AU)')
-
-ax.set_xlim(alphas.index.min(), alphas.index.max())
+ax = axs[6]
+ax.plot(alphas['Status'], alpha=0.4, lw=0, marker='o')
 
 plt.show()
